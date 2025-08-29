c8b79a773096e945fb654b3366ebea2fa646b181
Jsons21 <jakesonsini@gmail.com>
2025-08-28 21:49:01 -0400

    employeelist

---
+++ app.js
@@ -0,0 +1,33 @@
+import express from "express";
+import employees from "./db/employees.js";
+
+const app = express();
+
+export default app;
+
+app.route("/").get((req, res) => {
+  res.send("Hello employees!");
+});
+
+app.route("/employees").get((req, res) => {
+  res.send(employees);
+});
+
+app.route("/employees/random").get((req, res) => {
+  const id = Math.floor(Math.random(employees.length) * employees.length);
+  res.send(employees[id]);
+});
+
+app.route("/employees/:id").get((req, res) => {
+  const { id } = req.params;
+  console.log(!isNaN(id));
+  if (isNaN(id)) {
+    return res.status(400).send("ID Needs To Be A Number");
+  }
+
+  if (id > employees.length - 1) {
+    return res.status(400).send("Employee not found");
+  }
+
+  res.send(employees[id]);
+});
